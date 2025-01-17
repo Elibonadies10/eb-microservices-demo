c5f98f7256c8ebada480736588b461daeb577277
Elibonadies10 <62408029+Elibonadies10@users.noreply.github.com>
2025-01-17 10:24:50 -0500

    profiling

--- java-service/src/main/java/com/example/Application.java
+++ java-service/src/main/java/com/example/Application.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.context.annotation.Bean;
+import jakarta.annotation.PostConstruct;
+import io.pyroscope.javaagent.PyroscopeAgent;
+import io.pyroscope.javaagent.config.Config;
+import io.pyroscope.javaagent.EventType;
+import io.pyroscope.http.Format;
 
 @SpringBootApplication
 public class Application {
@@ -14,5 +19,19 @@ public class Application {
     @Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
+    }
+
+    @PostConstruct
+    public void init() {
+        PyroscopeAgent.start(
+            new Config.Builder()
+                .setApplicationName("eb-java-service")
+                .setProfilingEvent(EventType.ITIMER)
+                .setFormat(Format.JFR)
+                .setServerAddress(System.getenv("PYROSCOPE_SERVER_ADDRESS"))
+                .setBasicAuthUser(System.getenv("PYROSCOPE_USERNAME"))
+                .setBasicAuthPassword(System.getenv("PYROSCOPE_PASSWORD"))
+                .build()
+        );
     }
 }
